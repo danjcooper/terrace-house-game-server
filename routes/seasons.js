3d3ada60449e6db66410e2283a0b61aa3fb81307
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-01-10 18:14:21 +0000

    update seasons routes

--- routes/seasons.js
+++ routes/seasons.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const seasonsController = require('../controllers/seasons');
 
-router.get('/all', seasonsController.getAllSeasons);
+router.get('/', seasonsController.getAllSeasons);
 router.get('/:season', seasonsController.getSeasonHousemates);
 
 module.exports = router;
