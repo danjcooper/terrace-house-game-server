f0a5fe17e14a1578c81f9702b4a1099f419d01af
Dan Cooper <67024195+danjcooper@users.noreply.github.com>
2022-04-04 18:29:52 +0100

    update seasons model

--- models/seasons.js
+++ models/seasons.js
@@ -3,13 +3,25 @@ const dbQueries = require('../DB/queries/dbQueries');
 const getData = require('../data/parseData');
 
 class Seasons {
-  constructor(data) {}
+  constructor(data) {
+    this.id = data.seasonid;
+    this.name = data.seasonname;
+    this.totalCouples = data.couplecount;
+    this.totalHousemates = data.housematecount;
+    this.totalDates = data.datecount;
+    this.seasonLength = data.lengthofseason;
+  }
+
+  static create(data) {
+    return new Seasons(data);
+  }
 
   static get all() {
     return new Promise(async (resolve, reject) => {
       try {
-        const result = dbQueries.getAllSeasons(db);
-        resolve(result);
+        const result = await dbQueries.getAllSeasons(db);
+        const output = result.map((season) => Seasons.create(season));
+        resolve(output);
       } catch (error) {
         reject(error);
       }
